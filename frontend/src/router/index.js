06fd503cc4e600debfae76421b6c269e95c69415
Pepo <pepo.ospina@gmail.com>
2018-03-11 16:54:57 +0100

    moving to hash history (auth 0 lock)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,7 +24,7 @@ import AnswerPoll from '@/components/answerpoll/AnswerPoll'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   routes: [
     {
       path: '/',
