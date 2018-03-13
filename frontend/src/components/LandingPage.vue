36e809287d7d5080685425b6eee2ecd66dea2d74
Pepo <pepo.ospina@gmail.com>
2018-03-13 22:09:05 +0100

    style

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,18 +1,20 @@
 
 <template lang="html">
   <div class="content-container this-container">
-    <div class="central-column-container dark-1">
-      <div class="w3-row w3-center">
+    <div class="gradient-container">
+      <div class="central-column-container">
+        <div class="w3-row w3-center">
 
-      </div>
-      <div class="w3-row w3-center">
-        <img class="logo-landing" src="./../assets/logo-white.png" alt="">
-      </div>
-      <div class="w3-row btn-row">
-        <app-button @click="start()"
-            class="margin-0-auto">{{ $t('START') }}</app-button>
-      </div>
-      <div class="w3-row w3-center">
+        </div>
+        <div class="w3-row w3-center">
+          <img class="logo-landing" src="./../assets/logo-white.png" alt="">
+        </div>
+        <div class="w3-row btn-row">
+          <app-button @click="start()"
+          class="margin-0-auto">{{ $t('START') }}</app-button>
+        </div>
+        <div class="w3-row w3-center">
+        </div>
       </div>
     </div>
   </div>
@@ -42,6 +44,13 @@ export default {
   background: url('./../assets/background-2.png');
   background-color: #3F3E3E;
   height: 100vh;
+  padding: 0px;
+}
+
+.gradient-container {
+  background: radial-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.0));
+  height: 100vh;
+  padding-top: 15vh;
 }
 
 .central-column-container {
@@ -49,7 +58,7 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   height: 70vh;
-}
+ }
 
 .btn-row {
   margin-top: 35px;
